26d5bb9e8d17d05efa20aa9f7050e8620f30e400
Katumba <jonathanpkatumba@gmail.com>
2022-10-26 10:44:38 +0000

    added DepositSlot class

---
+++ DepositSlot.java
@@ -0,0 +1,9 @@
+public class DepositSlot
+{
+    // indicates whether envelope was received (always returns true,
+    // because this is only a software simulation of a real deposit slot)
+    public boolean isEnvelopeReceived()
+    {
+        return true; // deposit envelope was received
+    } // end method isEnvelopeReceived
+} 
